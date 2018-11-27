2010bc3d48d88ac2853055d52215b49210262ff4
Raja K <raja.k@heptagon.in>
2018-11-27 17:11:56 +0530

    no message

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ require('electron-reload')(__dirname, {
 // keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
+// defines percentage of app screen size
 let progress=20;
 
 function createWindow() {
